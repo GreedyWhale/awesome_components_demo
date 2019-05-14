7c8346e1dbdf476cc3a60f687b8b3b96e52694fe
caisr <383911973@qq.com>
2019-05-14 20:02:16 +0800

    init

---
+++ pages/avatar/avatar.js
@@ -0,0 +1,12 @@
+Page({
+  data: {
+    url: "http://img1.imgtn.bdimg.com/it/u=3445541619,4123984622&fm=26&gp=0.jpg",
+    tag: "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2297737617,2900544406&fm=26&gp=0.jpg",
+    urls: [
+      "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1151206697,3011049610&fm=26&gp=0.jpg",
+      "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3239610548,2829365940&fm=26&gp=0.jpg",
+      "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1214200266,2897760271&fm=26&gp=0.jpg",
+      "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2297737617,2900544406&fm=26&gp=0.jpg"
+    ]
+  }
+})
---
+++ pages/home/home.js
@@ -0,0 +1,21 @@
+Page({
+  data: {
+    components: [
+      {name: '头像', desc: 'Avatar', icon: 'icon-avatar', goNextPath: '/pages/avatar/avatar'},
+      {name: '加载', desc: 'Loading', icon: 'icon-loading', goNextPath: '/pages/loading/loading'},
+      {name: '标签页', desc: 'Tabs', icon: 'icon-tabs', goNextPath: ''},
+      {name: '垂直导航', desc: 'Vertical Navigation', icon: 'icon-v-nav', goNextPath: ''},
+      {name: '水印生成器', desc: 'Watermark', icon: 'icon-watermark', goNextPath: ''},
+      {name: '时间线', desc: 'Timeline', icon: 'icon-timeline', goNextPath: ''},
+      {name: '进度条', desc: 'Progress', icon: 'icon-progress', goNextPath: ''},
+      {name: '弹窗', desc: 'Dialog', icon: 'icon-dialog', goNextPath: ''},
+      {name: '自定义导航栏', desc: 'Nav Bar', icon: 'icon-nav-bar', goNextPath: ''},
+      {name: '通知栏', desc: 'Notice Bar', icon: 'icon-notice', goNextPath: ''},
+      {name: '数字选择器', desc: 'Number Selector', icon: 'icon-number-selector', goNextPath: ''},
+      {name: '滑块', desc: 'Slide View', icon: 'icon-slide-view', goNextPath: ''},
+      {name: '步骤条', desc: 'Steps', icon: 'icon-steps', goNextPath: ''},
+      {name: '吸顶', desc: 'Sticky', icon: 'icon-sticky', goNextPath: ''},
+      {name: '点赞', desc: 'Give Like', icon: 'icon-like', goNextPath: ''},
+    ]
+  }
+})
